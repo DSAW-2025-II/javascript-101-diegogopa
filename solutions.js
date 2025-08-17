d745e5a12dea0b0a6b90803c59c02634766bcf25
Diego Alejandro Gómez <diegogopa@unisabana.edu.co>
2025-08-17 18:07:12 -0500

    Segundo intento

--- solutions.js
+++ solutions.js
@@ -1,6 +1,10 @@
 // Sum of Two Numbers
 function sum(a, b) {
   // Add your code here
+  if (typeof a !== "number" || typeof b !== "number") {
+  console.log("Los datos de entrada no son adecuados");
+  return;
+}
   return a+b;
 }
 
@@ -10,18 +14,26 @@ function factorial(n) {
   if(n<0){
     return undefined;
   }
+  if(n=0){
+    return 1;
+  }
   else{
-    for(let rep=1;rep<=n;rep++){
-      let result=1
-      result=result*rep;
+    for(let rep=n-1;rep==1;rep--){
+      n=n*rep;
     }
-    return result;
+    return n;
   }
 }
 
 // Find the Largest Number
 function findLargest(arr) {
   // Add your code here
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number") {
+      console.log("Los datos de entrada no son adecuados");
+      return;
+    }
+  }
   let max = arr[0]; // empezamos con el primer número
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > max) {
@@ -35,7 +47,11 @@ function findLargest(arr) {
 // Count Vowels in a String
 function countVowels(str) {
   // Add your code here
-   let count = 0;
+   if (typeof str !== "string") {
+    console.log("Los datos de entrada no son adecuados");
+    return;
+  }
+  let count = 0;
   let vowels = "aeiouAEIOU"; // incluimos mayúsculas
   for (let i = 0; i < str.length; i++) {
     if (vowels.includes(str[i])) {
@@ -48,6 +64,10 @@ function countVowels(str) {
 // Check if a Number is Prime
 function isPrime(n) {
   // Add your code here
+   if (typeof n !== "number") {
+    console.log("Los datos de entrada no son adecuados");
+    return;
+  }
   if (n <= 1) return false; // 0 y 1 no son primos
   for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
